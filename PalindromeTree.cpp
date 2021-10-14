27530c0a073148e63d81c3cf449585c992464fc7
ninanguyen2708 <61177527+ninanguyen2708@users.noreply.github.com>
2021-10-14 18:29:53 +0700

    Create PalindromeTree.cpp

---
+++ PalindromeTree.cpp
@@ -0,0 +1,50 @@
+#include "bits/stdc++.h" 
+using namespace std; 
+
+#define MAXN1000 
+struct Node { 
+  int start, end; int length; int insertEdg[26]; int suffixEdg; 
+}; 
+
+Node root1, root2, tree[MAXN]; 
+int currNode; string s; int ptr; 
+
+void insert(int idx) { 
+  int tmp = currNode; 
+  while (true) { 
+    int curLength = tree[tmp].length; 
+    if (idx -curLength >= 1 and s[idx]== s[idx-curLength-1]) break; 
+    tmp = tree[tmp].suffixEdg; 
+  } 
+  if(tree[tmp].insertEdg[s[idx]-'a'] != 0) { currNode = tree[tmp].insertEdg[s[idx]-'a']; return; } 
+  ptr++; 
+  tree[tmp].insertEdg[s[idx]-'a'] = ptr; 
+  tree[ptr].length = tree[tmp].length + 2; 
+  tree[ptr].end = idx; 
+  tree[ptr].start = idx -tree[ptr].length + 1;
+  tmp = tree[tmp].suffixEdg; 
+  currNodecurrNode = ptr; 
+  if (tree[currNode].length == 1) { tree[currNode].suffixEdg = 2; return; } 
+  while (true) { 
+    int curLength = tree[tmp].length; 
+    if (idx-curLength >= 1 and s[idx] == s[idx-curLength-1]) break; 
+    tmp = tree[tmp].suffixEdg; 
+  } 
+  tree[currNode].suffixEdg = tree[tmp].insertEdg[s[idx]-'a']; 
+} 
+
+void solve() {
+  root1.length = -1; root1.suffixEdg = 1; root2.length = 0; root2.suffixEdg = 1; 
+  tree[1] = root1; tree[2] = root2; ptr = 2; currNode = 1;  
+  int l = s.length(); 
+  for (int i=0; i<l; i++) insert(i); 
+  cout << "All distinct palindromic substring for "<< s << " : \n"; 
+  for (int i=3; i<=ptr; i++) { 
+    cout << i-2 << ") "; 
+    for (int j=tree[i].start; j<=tree[i].end; j++) cout << s[j]; cout << endl; 
+  } 
+}
+int main() { 
+  solve();
+  return 0; 
+}
