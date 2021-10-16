7bb092626471c2df3ce597a4fff0310795188af2
ninanguyen2708 <61177527+ninanguyen2708@users.noreply.github.com>
2021-10-16 08:51:13 +0700

    Create imsolazy.cpp

---
+++ imsolazy.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+
+#define sz(x) (int)x.size()
+#define all(x) begin(x), end(x)
+
+const int MOD = 1000000007; // 998244353
+const ll INF = 1e18;
+const int oo = 1000000007;
+const int MX = 100001;
+const ld PI = 4 * atan((ld)1);
+
+void solve() {
+	
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	solve();
+	return 0;
+}
