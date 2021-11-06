953a2ae7db0bb106929b08d3966b8de5d75b719c
ninanguyen2708 <61177527+ninanguyen2708@users.noreply.github.com>
2021-11-06 22:56:58 +0700

    Update Segment Tree.cpp

--- Segment Tree.cpp
+++ Segment Tree.cpp
@@ -1,20 +1,20 @@
 /*  ID, comb, T can change
- *	this is sum
+ *	this is sum -> ID = 0
+ *		min -> ID = 1e18
  */
-Template<class T> struct Seg {
-	const T ID = 0; T comb(T a, T b) { return a + b; }
+template<class T> struct Seg { // comb(ID,b) = b
+	const T ID = 0 /*ID{}*/; T comb(T a, T b) { return min(a,b); }
 	int n; vector<T> seg;
-	void init(int _n) { n_ = n; vector<T> seg(4 * n + 7, ID); }
-	void pull(int p) { seg[p] = comb(seg[2 * p], seg[2 * p + 1]);
-	void upd(int p, T val) {
-		seg[p += n] = val; for (p /= 2; p; p /= 2) pull(p);
-	}
-	T query(int l, int r) {
-		T u = ID; T v = ID;
-		for (l += n; r += n + 1; l < r; l /= 2; r /= 2) {
-			if (l&1) u = comb(u, seg[l++]);
-			if (r&1) v = comb(seg[r--], v);
+	void init(int _n) { n = _n; seg.assign(4 * n + 7,ID); }
+	void pull(int p) { seg[p] = comb(seg[2 * p],seg[2 * p + 1]); }
+	void upd(int p, T val) { // set val at position p
+		seg[p += n] = val; for (p /= 2; p; p /= 2) pull(p); }
+	T query(int l, int r) {	// min on interval [l, r]
+		T ra = ID, rb = ID;
+		for (l += n, r += n+1; l < r; l /= 2, r /= 2) {
+			if (l&1) ra = comb(ra,seg[l++]);
+			if (r&1) rb = comb(seg[--r],rb);
 		}
-		return comb(u, v);
+		return comb(ra, rb);
 	}
-}
+};
