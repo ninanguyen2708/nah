5433f961cccc33a26a05c8d7e82aa0e2228169d2
ninanguyen2708 <61177527+ninanguyen2708@users.noreply.github.com>
2021-12-17 16:34:57 +0700

    Update Segment Tree.cpp

--- Segment Tree.cpp
+++ Segment Tree.cpp
@@ -18,45 +18,40 @@ template<class T> struct Seg { // comb(ID,b) = b
 		return comb(ra, rb);
 	}
 };
-/* ------original------ 
+/* ------------ 
  * Des: Number of Minimums on a Segment 
  * https://codeforces.com/edu/course/2/lesson/4/1/practice/contest/273169/problem/C
  */
-template<class T> struct Seg { //[0, n]
-	const T ID = mp(2e9, 0); int size; vector<T> seg; //*
-	T comb(T a, T b) { //*
+template<class T> struct Seg { // 0-indexed
+	const T ID = mp(2e9, 0); int n; vector<T> seg; 
+
+	T comb(T a, T b) { 
 		if (a.f < b.f) return a; 
 		if (a.f > b.f) return b;
 		if (a.f == b.f) return mp(a.f, a.s + b.s); 
 	}
+	
 	void init(int _n) {
-		size = 1; while (size < _n) size *= 2;
-		seg.assign(2 * size, ID);
+		for (n = 1; n < _n;) n *= 2;
+		seg.assign(2 * n, ID);
 	}
-	void build(vi &a, int x, int Lx, int Rx) {
-		if (Rx - Lx == 1) { if (Lx < sz(a)) seg[x] = mp(a[Lx], 1); return; }
-		int m = (Lx + Rx) >> 1;
-		build(a, 2 * x + 1, Lx, m);
-		build(a, 2 * x + 2, m, Rx);
-		seg[x] = comb(seg[2 * x + 1], seg[2 * x + 2]);
+
+	void build(vi &a) {
+		for (int i = 0; i < sz(a); i++) seg[i + sz(a)] = mp(a[i], 1);
+		for (int i = n - 1; i > 0; --i) seg[i] = comb(seg[i >> 1], seg[i >> 1 | 1]);
 	}
-	void upd(int i, int val, int x, int Lx, int Rx) {
-		if (Rx - Lx == 1) { seg[x] = mp(val, 1); return; }
-		int m = (Lx + Rx) >> 1;
-		if (i < m) 	upd(i, val, 2 * x + 1, Lx, m);
-		else 		upd(i, val, 2 * x + 2, m, Rx);
-		seg[x] = comb(seg[2 * x + 1], seg[2 * x + 2]);
+
+	void upd(int p, int val) {
+		for (seg[p += n] = mp(val, 1); p > 1; p >>= 1) seg[p >> 1] = comb(seg[p], seg[p ^ 1]);
 	}
-	T query(int l, int r, int x, int Lx, int Rx) {
-		if (r <= Lx) return ID; if (l >= Rx) return ID;
-		if (l <= Lx && Rx <= r) return seg[x];
-		int m = (Lx + Rx) >> 1;
-		T s1 = query(l, r, 2 * x + 1, Lx, m);
-		T s2 = query(l, r, 2 * x + 2, m, Rx);
-		return comb(s1, s2);
+
+	T query(int l, int r) {
+		T ra = ID, rb;
+		for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
+		    if (l&1) ra = comb(ra, seg[l++]);
+		    if (r&1) rb = comb(seg[--r], rb);
+		}
+		return comb(ra, rb);
 	}
-	void build(vector<int> &a) { build(a, 0, 0, size); }
-	void upd(int i, int val) { upd(i, val, 0, 0, size); }
-	T query(int l, int r) { return query(l, r, 0, 0, size); }
-}; 
-int n, m; vi a; Seg<pi> seg;
+};
+
