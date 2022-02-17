42be82df147a07c50cf5a0e319ca2e21c4906088
Qkake <61177527+ninanguyen2708@users.noreply.github.com>
2022-02-17 07:49:33 +0700

    Update imsolazy.cpp

--- imsolazy.cpp
+++ imsolazy.cpp
@@ -49,15 +49,11 @@ const int oo = 1000000007;
 const int MX = 100001;
 const ld PI = 4 * atan((ld)1);
 
+tcT> T pw(T a, T b, T m) {
+	T res=1; a %= m; while (b>0) { if(b&1) (res*=a)%=m; (a*=a)%m; b>>=1; } 
+	return res; }
 tcT> bool maximize(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
 tcT> bool minimize(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
-tcT> T pw(T a, T b, T m) {
-	T res = 1; a = a % m;
-	while (b > 0) {
-		if (b&1) res = (res * a) % m;
-		a = (a * a) % m; b >>= 1; 
-	} return res;
-}
 
 void solve() {
 	
